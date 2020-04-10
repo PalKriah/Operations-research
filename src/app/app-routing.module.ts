a73d8c478b5958241ef82e768c790348b796e63c
Palanek Krisztián <pal.kriah@gmail.com>
2020-04-10 03:01:07 +0200

    Define routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SimplexSolverComponent } from './components/simplex-solver/simplex-solver.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SimplexSolverComponent },
+  { path: 'simplex', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
