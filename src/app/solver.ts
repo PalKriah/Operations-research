50c774489adedf567b829991665ea99963abd23c
Palanek Krisztián <pal.kriah@gmail.com>
2020-04-10 03:05:42 +0200

    Adding main class for the solver

---
+++ src/app/solver.ts
@@ -0,0 +1,12 @@
+import { ProblemType } from './data_structures/ProblemType';
+import { Restriction } from './data_structures/Restriction';
+
+export class Solver {
+    private problemType: ProblemType;
+    private goalFunction: number[];
+    private restrictions: Restriction[];
+
+    constructor() {
+
+    }
+}
